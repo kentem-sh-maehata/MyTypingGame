a8e802852ce19d27febfcc4a3e24053bf5eeda99
kentem-sh-maehata <sh-maehata@kentem@.co.jp>
2024-05-28 16:22:40 +0900

    キーダウンして画面に反映

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 "use strict";
 {
+    const word = document.getElementById("word");
+    const words = ["pink", "blue", "red"];
+    let idx = 0;
+    word === null || word === void 0 ? void 0 : word.addEventListener("click", () => {
+        // gameProcess()
+        if (word)
+            word.textContent = words[idx];
+    });
+    let wordArray = words[idx].split("");
+    let loc = 0;
+    // console.log(words[idx])
+    document.addEventListener("keydown", (e) => {
+        console.log(e.key);
+        console.log(wordArray[loc]);
+        if (e.key === wordArray[loc]) {
+            wordArray[loc] = "_";
+            if (word)
+                word.innerHTML = wordArray.join('');
+            loc++;
+            if (loc === wordArray.length) {
+                idx++;
+                if (word)
+                    word.textContent = words[idx];
+                wordArray = words[idx].split("");
+                loc = 0;
+            }
+        }
+    });
 }
--- main.ts
+++ main.ts
@@ -1,3 +1,36 @@
 {
+  const word = document.getElementById("word")
+  const words:string[] = ["pink","blue","red"]
+  let idx = 0
+
+  word?.addEventListener("click",()=>{
+    // gameProcess()
+    if(word)
+      word.textContent = words[idx]
+  })
+  let wordArray = words[idx].split("")
+  let loc = 0
+    // console.log(words[idx])
+  
+  
+  document.addEventListener("keydown",(e)=>{
+    console.log(e.key)
+    console.log(wordArray[loc])
+    if(e.key === wordArray[loc]){
+      wordArray[loc] = "_"
+      if(word)
+        word.innerHTML = wordArray.join('')
+
+      loc ++
+      if(loc === wordArray.length){
+        idx ++
+        if(word)
+          word.textContent = words[idx]
+        wordArray = words[idx].split("")
+        loc = 0
+      }
+    }
+  })
+
   
 }
